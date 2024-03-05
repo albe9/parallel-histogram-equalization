cf0b44b17be09d47ccec447701c90871feb2eff1
albe9 <albertom1999@gmail.com>
2024-03-05 10:09:54 +0100

    update working tiling

--- src/main.cu
+++ src/main.cu
@@ -124,42 +124,140 @@ __global__ void clahe_kernel(uint8_t* in_image, uint8_t* out_image, uint32_t wid
     }
 }
 
-__global__ void clahe_kernel_shared_mem(uint8_t* in_image, uint8_t* out_image, uint32_t width, uint32_t height, uint32_t clip_limit, const uint32_t tileRadius, uint32_t n_pixel_to_load_4_thread)
+__global__ void clahe_kernel_shared_mem(uint8_t* in_image, uint8_t* out_image, uint32_t width, uint32_t height, uint32_t clip_limit, const uint32_t claheRadius, uint32_t n_pixel_to_load_4_thread)
 {
-    if(blockIdx.x == 0 && blockIdx.y == 1 && threadIdx.x == 3 && threadIdx.y == 3)
+    //_______________________________________________________________________________________________________________
+    // FIRST PHASE: load all pixel needed for the current block in shared memory
+
+    extern __shared__ uint8_t tile_shared[];
+    uint32_t pixel_block_num = threadIdx.y * blockDim.x + threadIdx.x;
+    uint32_t tile_dim_x = (2 * claheRadius + blockDim.x);
+    uint32_t tile_dim_y = (2 * claheRadius + blockDim.y);
+    uint32_t mirrored_width = 2 * claheRadius + width,
+            mirrored_height = 2 * claheRadius + height;
+    // calculating range of pixel that current thread need to load 
+    uint32_t start_load_index = pixel_block_num * n_pixel_to_load_4_thread;
+    uint32_t end_load_index = 0;
+    if(pixel_block_num == blockDim.x * blockDim.y - 1)
     {
-        //_______________________________________________________________________________________________________________
-        // FIRST PHASE: load all pixel needed for the current block in shared memory
+        //pixels to load are divided equally, last thread load also remaining pixels
+        end_load_index = tile_dim_x * tile_dim_y;
+    }
+    else
+    {
+        end_load_index = (pixel_block_num +1) * n_pixel_to_load_4_thread;
+    }
+    
+    for(uint32_t pixel_to_load_idx =  start_load_index; pixel_to_load_idx < end_load_index; pixel_to_load_idx++ )
+    {
+        // mapping pixel_to_load_idx to pixel idx relative to in_image
+        uint32_t pixel_idx_y = pixel_to_load_idx / tile_dim_x + blockIdx.y * blockDim.y;
+        uint32_t pixel_idx_x = pixel_to_load_idx % tile_dim_x + blockIdx.x * blockDim.x;
+        uint32_t abs_pixel_idx = pixel_idx_y * mirrored_width + pixel_idx_x;
+        //handle right and bottom blocks (to avoid accessing pixels that don't belong to the image)
+        if(pixel_idx_x < mirrored_width && pixel_idx_y < mirrored_height)
+        {
+            tile_shared[pixel_to_load_idx] = in_image[abs_pixel_idx];
+        }
+    }
 
-        extern __shared__ uint8_t Ms[];
-        uint32_t pixel_block_num = threadIdx.y * blockDim.x + threadIdx.x;
-        uint32_t shared_m_dim_x = (2 * tileRadius + blockDim.x);
-        uint32_t shared_m_dim_y = (2 * tileRadius + blockDim.y);
-        uint32_t mirrored_width = 2 * tileRadius + width,
-             mirrored_height = 2 * tileRadius + height;
-        // calculating range of pixel that current thread need to load 
-        uint32_t start_load_index = pixel_block_num * n_pixel_to_load_4_thread;
-        uint32_t end_load_index = 0;
-        if(pixel_block_num == blockDim.x * blockDim.y - 1)
+    __syncthreads();
+    //_______________________________________________________________________________________________________________
+    // SECOND PHASE: calculate clahe
+
+    // avoid pixels out of image
+    uint32_t main_pixel_idx_y = blockDim.y * blockIdx.y + threadIdx.y;
+    uint32_t main_pixel_idx_x = blockDim.x * blockIdx.x + threadIdx.x;
+    if(main_pixel_idx_x < width && main_pixel_idx_y < height)
+    {
+        // Iterate through pixels in the current tile and calculate histogram
+        uint32_t histogram[GRAYSCALE_RANGE] = {0};
+        for(uint32_t tile_pixel_y = threadIdx.y; tile_pixel_y < 2 * claheRadius + 1 + threadIdx.y; tile_pixel_y++)
         {
-            //pixels to load are divided equally, last thread load also remaining pixels
-            end_load_index = shared_m_dim_x * shared_m_dim_y;
-        }
-        else
-        {
-            end_load_index = (pixel_block_num +1) * n_pixel_to_load_4_thread;
-        }
-        
-        for(uint32_t pixel_to_load_idx =  start_load_index; pixel_to_load_idx < end_load_index; pixel_to_load_idx++ )
-        {
-            // calculating pixel idx relative to in_image
-            uint32_t abs_pixel_idx = (pixel_to_load_idx / shared_m_dim_x + blockIdx.y * blockDim.y) * mirrored_width +
-                                     pixel_to_load_idx % shared_m_dim_x + blockIdx.x * blockDim.x;
-            // TODO handle right and bottom blocks (where)
-            printf("%d ", in_image[abs_pixel_idx]);
+            for(uint32_t tile_pixel_x = threadIdx.x; tile_pixel_x < 2 * claheRadius + 1 + threadIdx.x; tile_pixel_x++) 
+            {
+                uint8_t pixelValue = tile_shared[tile_pixel_y * tile_dim_x + tile_pixel_x];
+                histogram[pixelValue]++;
+            }
         }
 
-        //_______________________________________________________________________________________________________________
+        //TESTING
+        // if(blockIdx.x == 50 && blockIdx.y == 50 && threadIdx.x == 0 && threadIdx.y == 0)
+        // {
+        //     for(uint32_t pixel_value=0; pixel_value < GRAYSCALE_RANGE; pixel_value++)
+        //     {
+        //         printf("%d ", histogram[pixel_value]);
+        //     }
+        //     printf("\n");
+        // }
+
+        // Calculate max and min value in histogram to convert clip limit
+        uint32_t max_level_value = 0, min_level_value = 1000000;
+        for(uint32_t level = 0; level < GRAYSCALE_RANGE; level++)
+        {
+            uint32_t level_value = histogram[level];
+            if(level_value < min_level_value) min_level_value = level_value;
+            if(level_value > max_level_value) max_level_value = level_value;
+        }
+
+        uint32_t scaled_clip_limit = min_level_value + (clip_limit * (max_level_value - min_level_value) / 10); 
+
+        // Clip histogram values above the clip limit
+        uint32_t excess = 0;
+        for(uint32_t level = 0; level < GRAYSCALE_RANGE; level++)
+        {
+            if (histogram[level] > scaled_clip_limit)
+            {
+                excess += histogram[level] - scaled_clip_limit;
+                histogram[level] = scaled_clip_limit;
+            }
+        }
+
+        // Distribute the excess counts uniformly among all histogram bins
+        uint32_t window_cdf[GRAYSCALE_RANGE] = {0}, cdf_counter = 0;
+        uint32_t bin_increment = excess / GRAYSCALE_RANGE;
+        uint32_t remainder = excess % GRAYSCALE_RANGE;
+        for(uint32_t level = 0; level < GRAYSCALE_RANGE; level++)
+        {
+            histogram[level] += bin_increment;
+        }
+        for(uint32_t level = 0; level < remainder; level++)
+        {
+            histogram[level]++;
+        }
+
+        // calculate window cdf
+        uint32_t min_cdf = 0;
+        for(uint32_t level = 0; level < GRAYSCALE_RANGE; level++)
+        {
+            cdf_counter += histogram[level];
+            window_cdf[level] = cdf_counter;
+            if(min_cdf == 0 && cdf_counter != 0) min_cdf = cdf_counter;
+        }
+
+        //TESTING
+        // if(blockIdx.x == 50 && blockIdx.y == 50 && threadIdx.x == 0 && threadIdx.y == 0)
+        // {
+        //     for(uint32_t pixel_value=0; pixel_value < GRAYSCALE_RANGE; pixel_value++)
+        //     {
+        //         printf("%d ", window_cdf[pixel_value]);
+        //     }
+        //     printf("\n");
+        // }
+
+        // Apply the contrast-limited histogram equalization to the main pixel
+
+        float pixel_for_window = (2 * claheRadius + 1) * (2 * claheRadius + 1);
+        uint32_t main_pixel_abs_pos = main_pixel_idx_y * width + main_pixel_idx_x;
+        uint32_t mirrored_pixel_abs_pos = (main_pixel_idx_y + claheRadius) * mirrored_width + main_pixel_idx_x + claheRadius;
+        uint8_t new_pixel_value = std::round(static_cast<double>(window_cdf[in_image[mirrored_pixel_abs_pos]] - min_cdf) / (pixel_for_window - min_cdf) * (GRAYSCALE_RANGE - 1));
+        out_image[main_pixel_abs_pos] = new_pixel_value;
+
+        //TESTING
+        // if(blockIdx.x == 70 && blockIdx.y == 50 && threadIdx.x == 0 && threadIdx.y == 0)
+        // {
+        //     printf("pixel pos : %d, cdf : %d, new_pixel_value : %d, min_cdf : %d\n", mirrored_pixel_abs_pos, window_cdf[in_image[mirrored_pixel_abs_pos]], new_pixel_value, min_cdf);
+        // }
     }
 }
 
@@ -232,7 +330,7 @@ void parallel_clahe(uint32_t clip_limit, uint32_t tileRadius, uint32_t iter_n)
 void parallel_clahe_shared_mem(uint32_t clip_limit, uint32_t tileRadius, uint32_t iter_n)
 {
     std::string in_img_path = "./../media/test3.jpg";
-    std::string out_img_path = "./../media/test3_output_cuda.jpg";
+    std::string out_img_path = "./../media/test3_output_cuda_shared.jpg";
 
     int width, height, channels;
     uint8_t *input_img = stbi_load(in_img_path.c_str(), &width, &height, &channels, 0);
@@ -268,40 +366,38 @@ void parallel_clahe_shared_mem(uint32_t clip_limit, uint32_t tileRadius, uint32_
     cudaMemcpy(gpu_input_img, in_mirrored_image, (mirrored_height) * (mirrored_width) * sizeof(uint8_t), cudaMemcpyHostToDevice);
 
     // Launch the kernel
-    uint32_t block_dim_x = 4, block_dim_y = 4;
+    uint32_t block_dim_x = 16, block_dim_y = 16;
     dim3 blockDim(block_dim_x, block_dim_y);
     dim3 gridDim((width + blockDim.x - 1) / blockDim.x, (height + blockDim.y - 1) / blockDim.y);
 
     uint32_t n_pixel_to_load_4_thread = ((2 * tileRadius + block_dim_x) * (2 * tileRadius + block_dim_y ))/ (block_dim_x * block_dim_y); 
 
     // TESTING_________________________________
-    for(uint32_t pixel_idx_y = 4; pixel_idx_y < 14; pixel_idx_y++)
-    {
-        for(uint32_t pixel_idx_x = 0; pixel_idx_x < 10; pixel_idx_x ++)
-        {
-            printf("%d ", in_mirrored_image[pixel_idx_y * mirrored_width + pixel_idx_x]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    //__________________________________________
-    clahe_kernel_shared_mem<<<gridDim, blockDim, (2 * tileRadius + block_dim_x) * (2 * tileRadius + block_dim_y ) * sizeof(uint8_t) >>>(gpu_input_img, gpu_output_img, width, height, clip_limit, tileRadius, n_pixel_to_load_4_thread);
-    cudaDeviceSynchronize();
-
-    // for(uint32_t iter_idx=0; iter_idx<iter_n; iter_idx++)
+    // for(uint32_t pixel_idx_y = 4; pixel_idx_y < 14; pixel_idx_y++)
     // {
-    //     clahe_kernel<<<gridDim, blockDim>>>(gpu_input_img, gpu_output_img, width, height, clip_limit, tileRadius);
-    //     cudaDeviceSynchronize();
-    //     if(iter_idx == iter_n - 1)
+    //     for(uint32_t pixel_idx_x = 0; pixel_idx_x < 10; pixel_idx_x ++)
     //     {
-    //         std::cout << "Iteration : ["<< iter_idx + 1 << "/" << iter_n << "]\n";
+    //         printf("%d ", in_mirrored_image[pixel_idx_y * mirrored_width + pixel_idx_x]);
     //     }
-    //     else
-    //     {
-    //         std::cout << "Iteration : ["<< iter_idx + 1 << "/" << iter_n << "]" << std::flush << "\r";
-    //     }
-        
+    //     printf("\n");
     // }
+    // printf("\n");
+    //__________________________________________
+
+    for(uint32_t iter_idx=0; iter_idx<iter_n; iter_idx++)
+    {
+        clahe_kernel_shared_mem<<<gridDim, blockDim, (2 * tileRadius + block_dim_x) * (2 * tileRadius + block_dim_y ) * sizeof(uint8_t) >>>(gpu_input_img, gpu_output_img, width, height, clip_limit, tileRadius, n_pixel_to_load_4_thread);
+        cudaDeviceSynchronize();
+        if(iter_idx == iter_n - 1)
+        {
+            std::cout << "Iteration : ["<< iter_idx + 1 << "/" << iter_n << "]\n";
+        }
+        else
+        {
+            std::cout << "Iteration : ["<< iter_idx + 1 << "/" << iter_n << "]" << std::flush << "\r";
+        }
+        
+    }
     
     cudaMemcpy(output_img, gpu_output_img, width * height * sizeof(uint8_t), cudaMemcpyDeviceToHost);
 
@@ -341,6 +437,15 @@ void cpu_clahe(uint8_t* in_image, uint8_t* out_image, uint32_t width, uint32_t h
                 }
             }
 
+            // TESTING
+            // if(main_pixel_y == tileRadius + 150 && main_pixel_x == tileRadius + 150)
+            // {
+            //     for(uint32_t pixel_value=0; pixel_value < GRAYSCALE_RANGE; pixel_value++)
+            //     {
+            //         printf("%d ", histogram[pixel_value]);
+            //     }
+            //     printf("\n");
+            // }
             // plotHist(histogram);
 
             // Calculate max and min value in histogram to convert clip limit
@@ -387,12 +492,27 @@ void cpu_clahe(uint8_t* in_image, uint8_t* out_image, uint32_t width, uint32_t h
                 if(min_cdf == 0 && cdf_counter != 0) min_cdf = cdf_counter;
             }
 
+            // TESTING
+            // if(main_pixel_y == tileRadius + 150 && main_pixel_x == tileRadius + 150)
+            // {
+            //     for(uint32_t pixel_value=0; pixel_value < GRAYSCALE_RANGE; pixel_value++)
+            //     {
+            //         printf("%d ", tile_cdf[pixel_value]);
+            //     }
+            //     printf("\n");
+            // }
             // plotHist(tile_cdf);
 
             // Apply the contrast-limited histogram equalization to the main pixel
             uint32_t main_pixel_abs_pos = main_pixel_y * mirrored_width + main_pixel_x;
             uint8_t new_pixel_value = std::round(static_cast<double>(tile_cdf[in_image[main_pixel_abs_pos]] - min_cdf) / (pixel_for_window - min_cdf) * (GRAYSCALE_RANGE - 1));
             out_image[(main_pixel_y - tileRadius) * width + (main_pixel_x - tileRadius)] = new_pixel_value;
+
+            // TESTING
+            // if(main_pixel_y == tileRadius + 150 && main_pixel_x == tileRadius + 210)
+            // {
+            //     printf("pixel pos : %d, cdf : %d, new_pixel_value : %d, min_cdf : %d\n", main_pixel_abs_pos, tile_cdf[in_image[main_pixel_abs_pos]], new_pixel_value, min_cdf);
+            // }
         }
     }
 }
@@ -432,19 +552,24 @@ void single_core_clahe(uint32_t clip_limit, uint32_t tileRadius)
 int main()
 {
     // CPU
-    // auto start_time = std::chrono::high_resolution_clock::now();
+    auto start_time = std::chrono::high_resolution_clock::now();
     // single_core_clahe(4, 20);
-    // auto end_time = std::chrono::high_resolution_clock::now();
-    // auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
     // std::cout << "Cpu version time elapsed : " << elapsed_time.count() << "\n";
 
     // GPU
-    // start_time = std::chrono::high_resolution_clock::now();
-    // parallel_clahe(4, 20, 85);
-    // end_time = std::chrono::high_resolution_clock::now();
-    // elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
-    // std::cout << "Gpu version time elapsed : " << elapsed_time.count() << "\n";
+    start_time = std::chrono::high_resolution_clock::now();
+    parallel_clahe(4, 20, 200);
+    end_time = std::chrono::high_resolution_clock::now();
+    elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+    std::cout << "Gpu version time elapsed : " << elapsed_time.count() << "\n";
 
     //GPU shared memory
-    parallel_clahe_shared_mem(4, 3, 85);
+    start_time = std::chrono::high_resolution_clock::now();
+    parallel_clahe_shared_mem(4, 20, 200);
+    end_time = std::chrono::high_resolution_clock::now();
+    elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+    std::cout << "Gpu shared memory version time elapsed : " << elapsed_time.count() << "\n";
+    
 }
