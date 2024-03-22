5f32da0aa510145b8f749deb08f2e9e107af6210
albe9 <albertom1999@gmail.com>
2024-03-22 19:45:29 +0100

    Added media for report

--- utils/graph_tracer.py
+++ utils/graph_tracer.py
@@ -1,10 +1,13 @@
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
+import matplotlib.gridspec as gridspec
+from PIL import Image
 import json
 import os
 
 ABS_PATH = os.path.abspath(__file__)
 BENCHMARKS_PATH = f"{os.path.abspath(os.path.join(os.path.dirname(ABS_PATH), os.pardir))}/benchmarks"
+MEDIA_PATH = f"{os.path.abspath(os.path.join(os.path.dirname(ABS_PATH), os.pardir))}/media"
 
 def trace_cpu_vs_gpu_graph(benchmarks_data):
     # setting number of img as x axis
@@ -44,7 +47,7 @@ def trace_cpu_vs_gpu_graph(benchmarks_data):
     labels.append('Speed_up')
 
     ax.legend(handles=handles, labels=labels)
-    fig.savefig(f"{BENCHMARKS_PATH}/benchmark_cpu_vs_gpu.png", dpi=600)
+    fig.savefig(f"{MEDIA_PATH}/benchmark_cpu_vs_gpu.png", dpi=600)
 
 def trace_gpu_shared_mem_graph(benchmarks_data):
     # setting number of img as x axis
@@ -70,8 +73,43 @@ def trace_gpu_shared_mem_graph(benchmarks_data):
     ax.set_title(f'CLAHE Benchmark GPU vs GPU with shared memory')
     ax.legend()
 
-    fig.savefig(f"{BENCHMARKS_PATH}/benchmark_gpu_mem_shared.png", dpi=600)
+    fig.savefig(f"{MEDIA_PATH}/benchmark_gpu_mem_shared.png", dpi=600)
 
+def trace_img_hist_comp(img_path_1, img_path_2):
+    img1 = Image.open(img_path_1)
+    pixel_values1 = list(img1.getdata())
+
+    img2 = Image.open(img_path_2)
+    pixel_values2 = list(img2.getdata())
+
+    # Create a 2x2 grid layout with custom sizes
+    fig = plt.figure(figsize=(10, 6))
+    gs = fig.add_gridspec(2, 2, width_ratios=[2, 2], height_ratios=[2, 1])
+
+    # Top left subplot
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax1.imshow(img1, cmap='gray')
+    ax1.set_title('Original image')
+    ax1.axis('off')
+
+    # Top right subplot
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax2.imshow(img2, cmap='gray')
+    ax2.set_title('Image after histogram equalization')
+    ax2.axis('off')
+
+    # Bottom left subplot
+    ax3 = fig.add_subplot(gs[1, 0])
+    ax3.hist(pixel_values1, 256)
+
+    # Bottom right subplot
+    ax4 = fig.add_subplot(gs[1, 1], sharey=ax3)
+    ax4.hist(pixel_values2, 256)
+
+    # Adjust layout to prevent overlap
+    plt.tight_layout()
+
+    fig.savefig(f"{MEDIA_PATH}/histogram_eq_sample.png", dpi=600)
 
 def main():
 
@@ -85,7 +123,7 @@ def main():
         benchmarks_data_graph1["gpu_mem_shared"] = json.load(json_file)
     
     #trace graph
-    trace_cpu_vs_gpu_graph(benchmarks_data_graph1)
+    # trace_cpu_vs_gpu_graph(benchmarks_data_graph1)
 
     # load benchmarks data
     benchmarks_data_graph2 = dict()
@@ -95,8 +133,9 @@ def main():
         benchmarks_data_graph2["gpu_mem_shared"] = json.load(json_file)
     
     #trace graph
-    trace_gpu_shared_mem_graph(benchmarks_data_graph2)
+    # trace_gpu_shared_mem_graph(benchmarks_data_graph2)
 
+    trace_img_hist_comp(f"{MEDIA_PATH}/test_img/test3.jpg", f"{MEDIA_PATH}/test_img/test3_output_cuda_shared.jpg")
 
 if __name__ == "__main__":
     main()
